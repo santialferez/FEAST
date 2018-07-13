076bbbfa8d884835935e710fc17f39b91db79393
santialferez <santiago.alferez@upc.edu>
2018-07-13 18:37:22 +0200

    Add discretization and (optionally) preprocessing to the feast class

--- python/feast.py
+++ python/feast.py
@@ -9,7 +9,7 @@ import numpy as np
 from numpy.ctypeslib import ndpointer
 import ctypes as c
 from sklearn.base import BaseEstimator, TransformerMixin
-
+from sklearn import preprocessing
 
 class feast(BaseEstimator, TransformerMixin):
     """ Class for FEAST features selection
@@ -20,6 +20,10 @@ class feast(BaseEstimator, TransformerMixin):
         ----------
         num_of_features : integer, default = 20
             the amount of features to choose.
+        scale_method : {None, 'softmax', 'quantile', 'robust'}, str, default = None
+            Feast needs discretize the continuous values (Real or float type),
+            The method discretize the values to 10 values (0 to 9). This parameter
+            controls the preprocessing method before this discretization.
 
         Attributes
         ----------
@@ -32,11 +36,11 @@ class feast(BaseEstimator, TransformerMixin):
             Indexes of selected feature.
         """
 
-    def __init__(self, num_of_features=20, criterion="CMIM"):
+    def __init__(self, num_of_features=20, criterion="CMIM", scale_method = None):
         if sys.platform.startswith('linux'):
             # in case of Linux OS load feast linux library (.so file)
             # self.libFSToolbox = c.CDLL('{}/{}/{}'.format(os.path.dirname(__file__), 'lib', 'libFSToolbox.so'))
-            self.libFSToolbox = c.CDLL("../libFSToolbox.so")
+            self.libFSToolbox = c.CDLL(os.path.dirname(__file__) + "/../libFSToolbox.so")
         elif sys.platform.startswith('win'):
             # in case of Windows OS load feast windows library (.dll file)
             self.libFSToolbox = c.WinDLL('{}/{}/{}'.format(os.path.dirname(__file__), 'lib', 'libFSToolbox.dll'))
@@ -49,6 +53,7 @@ class feast(BaseEstimator, TransformerMixin):
         self.feature_scores = None
         self.feature_names = None
         self.criterion = criterion # it could be CMIM, JMI or mRMR_D
+        self.scale_method = scale_method
 
     def fit(self, data, labels):
         """
@@ -68,6 +73,42 @@ class feast(BaseEstimator, TransformerMixin):
         if isinstance(data, pd.DataFrame):
             data_labels = data.columns.tolist()
 
+        ####### CODIFICATION AND DISCRETIZATION NECESSARY TO USE FEAST #########
+
+        # Codification from a string label to intengers
+        if labels.dtype != int:
+            encoder = preprocessing.LabelEncoder()
+            labels = encoder.fit_transform(labels)
+
+        # Normalization of data, necesary to the correct use of Feast
+
+        if self.scale_method == "softmax":
+            # Softmax
+            data = 1/( 1 - np.exp(-preprocessing.scale(data)) )
+
+        if self.scale_method == "quantile":
+            # Quantile transform
+            data = preprocessing.quantile_transform(data)
+
+        if self.scale_method == "robust":
+           # Robust scale_method
+           data = preprocessing.robust_scale(data)
+
+        # normalize each feature
+        data = preprocessing.minmax_scale(data)
+
+        # discretize each feature
+        n_bins = 10 # number of bins by default
+        X_n_samples, X_n_features = data.shape
+        X_discrete = np.zeros((X_n_samples, X_n_features))
+        bins = np.linspace(0, 1, n_bins)
+        for i in range(X_n_features):
+            X_discrete[:, i] = np.digitize(data[:, i], bins)
+        X_discrete = X_discrete-1
+
+        data = X_discrete
+        ########################################################################
+
         # python variables adaptation for C parameters initialization
         data = np.array(data, dtype=np.uint32, order="F")
         labels = np.array(labels, dtype=np.uint32)
@@ -85,11 +126,11 @@ class feast(BaseEstimator, TransformerMixin):
         c_class_column = labels.ctypes.data_as(c.POINTER(c.c_uint))
         c_output_features = output.ctypes.data_as(c.POINTER(c.c_uint))
         c_feature_scores = selected_features_score.ctypes.data_as(c.POINTER(c.c_double))
-        
+
         getattr(self.libFSToolbox,self.criterion).argtypes = [c.c_uint, c.c_uint, c.c_uint, _uintpp, c.POINTER(c.c_uint), c.POINTER(c.c_uint), c.POINTER(c.c_double)]
-        
+
         getattr(self.libFSToolbox,self.criterion).restype = c.POINTER(c.c_uint)
-        
+
         # call the C lib implementation
         c_selected_features = getattr(self.libFSToolbox,self.criterion)(
             c_k, c_no_of_samples, c_no_of_features, c_feature_matrix,
@@ -107,7 +148,7 @@ class feast(BaseEstimator, TransformerMixin):
 
         if data_labels is not None:
             self.feature_names = [data_labels[idx] for idx in self.feature_indexes]
-            
+
         return self
 
     def transform(self, X, y = None):
